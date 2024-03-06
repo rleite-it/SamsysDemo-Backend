6edd5f0a71eff44f59f25947cbc30fd4c6daee64
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-03-06 11:49:21 +0000

    Add birth date to client entity

--- SamsysDemo.Infrastructure/Entities/Client.cs
+++ SamsysDemo.Infrastructure/Entities/Client.cs
@@ -17,6 +17,8 @@ namespace SamsysDemo.Infrastructure.Entities
 
         public string Name { get; set; }
 
+        public DateTime BirthDate { get; set; }
+
         public string PhoneNumber { get; set; }
 
         public bool IsActive { get; set; }
